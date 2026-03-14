acaed1988c267c31e40b925cedd744c6d9c2afd8
khalid <khalid619lawal@gmail.com>
2026-03-14 14:51:14 +0000

    fix: rename tabular file

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,7 @@ import json
 from typing import Any, Dict
 
 from modelling import SchemaModeller
-from tabular import TabularAdapter
+from tabularize import TabularAdapter
 from vizualizer import SchemaViz
 
 
